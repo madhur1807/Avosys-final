9fa1c9e13c053b1ac8d9236daeda7a20ee86135d
madhur1807 <madhurmittal1807@gmail.com>
2018-02-14 21:07:37 +0530

    ...

---
+++ header.php
@@ -0,0 +1,65 @@
+
+<header class="header-area navbar-fixed-top">
+        <div class="container custom-header">
+        <div class="row">
+            <div id="menuzord" class="menuzord">
+                <a href="home.html" class="menuzord-brand"><img src="images/logo.png" width="150" alt=""></a>
+                    <div class="header-search">                       
+                    </div>                   
+                    <ul class="menuzord-menu menuzord-menu-bg">
+                    <li class="active"><a href="home.php">Home</a>                        
+                    </li>
+                    <li><a href="about-us.php">About Us</a>     
+                    </li>
+                    <!--Services-->
+                        <li><a href="#">Services</a>
+                        <ul class="dropdown">
+                            <li><a href="#">Retail Mortgage</a>
+                                <ul class="dropdown">
+                                    <li><a href="#">Services</a>
+                                        <ul class="dropdown">
+                                            <li><a href="home-loan.php">Home Loan</a></li>
+                                            <li><a href="loan-against-property.php">Loan Against Property</a></li>
+                                            <li><a href="purchase-of-commercial-property.php">Purchase of Commercial Property</a></li>
+                                            <li><a href="lease-rental-discounting.php">Lease Rental Discounting</a></li>
+                                            <li><a href="bussiness-loan.php">Business Loan</a></li>
+                                            <li><a href="balance-transfer.php">Balance Transfer</a></li>
+                                            <li><a href="emi.php">EMI Calculator</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">List of Documents</a>
+                                        <ul class="dropdown">
+                                            <li><a href="header-6.html">For Salaried</a></li>
+                                            <li><a href="header-7.html">For Professionals</a></li>
+                                            <li><a href="header-8.html">For Business</a></li>
+                                            <li><a href="header-9.html">For NRI</a></li>
+                                        </ul>
+                                    </li>
+                                    
+                                </ul>
+                            </li>
+                            <li><a href="#">Project Financing</a>
+                                        <ul class="dropdown">
+                                            <li><a href="working-capital.php">Working Capital</a></li>
+                                            <li><a href="term-loan.php">Term Loan</a></li>
+                                            <li><a href="balance-transfer.php">Balance Transfer</a></li>
+                                            <li><a href="bmi.php">EMI Calculator</a></li>
+                                        </ul>
+                                    </li>  
+                        </ul>
+                    </li>
+                        <!--end of services header-->
+                        
+                    
+                                            
+                    <li><a href="blog-grid.php">Get Associated With Us </a>
+                    </li>       
+                    <li><a href="contact.php">Contact Us</a></li>
+                    </ul>
+                    <div class="appointment-area">
+                        <p><a href="contact.php">Happy to help You Always !</a></p>
+                    </div>                     
+            </div>
+        </div>
+        </div>
+    </header>
---
+++ working-capital.php
@@ -0,0 +1,245 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- TITLE -->
+      <title>Working Capital</title>
+      <!-- FAVICON -->
+      <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />      
+      <!-- Bootstrap -->
+      <link href="css/bootstrap.min.css" rel="stylesheet">
+      <!--FONT AWESOME -->
+      <link href="css/font-awesome.css" rel="stylesheet">
+      <!-- MENU -->
+      <link rel="stylesheet" href="css/menuzord.css">       
+      <!-- STROKE GAP ICON -->
+      <link href="css/stroke-icon.css" rel="stylesheet">
+      <link href="css/demo.css" rel="stylesheet">
+      <link href="css/ie7.css" rel="stylesheet">
+      <!-- Portfolio Filter -->
+      <link rel="stylesheet" href="css/bootFolio.css">
+      <!-- Popup -->
+      <link rel="stylesheet" href="css/magnific-popup.css">      
+      <!-- JQUERY UI STYLE -->
+      <link rel="stylesheet" href="css/jquery-ui.css">        
+      <!-- OWL CSS -->
+      <link href="css/owl.theme.default.css" rel="stylesheet">
+      <link href="css/owl.carousel.css" rel="stylesheet">  
+      <!-- RS5.0 Main Stylesheet -->
+      <link rel="stylesheet" type="text/css" href="revolution/css/settings.css">    
+      <!-- RS5.0 Layers and Navigation Styles -->
+      <link rel="stylesheet" type="text/css" href="revolution/css/layers.css">
+      <link rel="stylesheet" type="text/css" href="revolution/css/navigation.css">           
+      <!-- MAIN STYLE -->
+      <link rel="stylesheet" href="css/style.css">
+
+      <!-- GOOGLE FONT -->
+      <link href='https://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900' rel='stylesheet' type='text/css'>
+      <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+      <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+        
+      <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+   </head>
+   <body>
+
+      <!-- =========================
+        START PRELOADER
+      ============================== -->
+      <div class="outslider_loading">
+         <div class="outslider_loader"></div>
+      </div>
+
+      <!-- =========================
+        END PRELOADER
+      ============================== -->
+      <!-- =========================
+        START HEADER SECTION
+      ============================== -->  
+      <html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- TITLE -->
+      <title>Avosys</title>
+      <!-- FAVICON -->
+      <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />      
+      <!-- Bootstrap -->
+      <link href="css/bootstrap.min.css" rel="stylesheet">
+      <!--FONT AWESOME -->
+      <link href="css/font-awesome.css" rel="stylesheet">
+      <!-- MENU -->
+      <link rel="stylesheet" href="css/menuzord.css">       
+      <!-- STROKE GAP ICON -->
+      <link href="css/stroke-icon.css" rel="stylesheet">
+      <link href="css/demo.css" rel="stylesheet">
+      <link href="css/ie7.css" rel="stylesheet">
+      <!-- Portfolio Filter -->
+      <link rel="stylesheet" href="css/bootFolio.css">
+      <!-- Popup -->
+      <link rel="stylesheet" href="css/magnific-popup.css">      
+      <!-- JQUERY UI STYLE -->
+      <link rel="stylesheet" href="css/jquery-ui.css">        
+      <!-- OWL CSS -->
+      <link href="css/owl.theme.default.css" rel="stylesheet">
+      <link href="css/owl.carousel.css" rel="stylesheet">  
+      <!-- RS5.0 Main Stylesheet -->
+      <link rel="stylesheet" type="text/css" href="revolution/css/settings.css">    
+      <!-- RS5.0 Layers and Navigation Styles -->
+      <link rel="stylesheet" type="text/css" href="revolution/css/layers.css">
+      <link rel="stylesheet" type="text/css" href="revolution/css/navigation.css">           
+      <!-- MAIN STYLE -->
+      <link rel="stylesheet" href="css/style.css">
+
+      <!-- GOOGLE FONT -->
+      <link href='https://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900' rel='stylesheet' type='text/css'>
+      <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+      <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+        
+      <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+   </head>
+   <body>
+
+      <!-- =========================
+        START PRELOADER
+      ============================== -->
+      <div class="outslider_loading">
+         <div class="outslider_loader"></div>
+      </div>
+
+      <!-- =========================
+        END PRELOADER
+      ============================== -->
+      
+      <!-- =========================
+        START HEADER SECTION
+      ============================== -->  
+
+      <!-- <php include "header.php"?> -->
+      <?php include 'header.php';?>
+
+      <!-- =========================
+        END HEADER SECTION
+      ============================== -->
+      <!-- =========================
+        END HEADER SECTION
+      ============================== -->
+
+      <!-- =========================
+        START PAGE TITLE SECTION
+      ============================== -->
+      <div class="page-title-area header-v-area">
+         <div class="container">
+            <div class="row">
+               <div class="col-sm-6 col-md-6">
+                  <div class="page-title-left">
+                     <h2>Working Capital</h2>
+                  </div>
+               </div>
+               <div class="col-sm-6 col-md-6">
+                  <div class="page-bredcrumbs-area text-right">
+                     <ul  class="page-bredcrumbs">
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Project Financing</a></li>
+                        <li><a href="#">Working Capital</a></li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- =========================
+        END PAGE TITLE SECTION
+      ============================== -->      
+
+      <!-- =========================
+        START FEATURE TYPOGRAPHY SECTION
+      ============================== -->         
+      <section class="feature-typo">
+           <div class="container">
+               <div class="row">
+                   <div class="col-md-12">
+                       <div class="f-h1-head">
+                           <h1>Working capital</h1>
+                           <p>Working capital is the amount of an entity's current assets minus its current liabilities. The result is considered a prime measure of the short-term liquidity of an organization. A strongly positive working capital balance indicates robust financial strength, while negative working capital is considered an indicator of impending bankruptcy.<br>A 2:1 ratio of current assets to current liabilities is considered healthy, though the ratio can vary by industry. The ratio may also be reviewed on a trend line, with the intent of spotting any declines or sudden drops that could indicate liquidity problems.<br><center><strong style="font-size:15px">Working Capital = CURRENT ASSETS - CURRENT LIABILITIES</strong></center></p>
+                       </div>
+                   </div>
+                   <div class="col-md-12">
+                       <div class="f-h1-head f-h2-head">
+                           <h2>Positive vs negative working capital</h2>
+                           <p>Having positive working capital can be a good sign of the short-term financial health for a company because it has enough liquid assets remaining to pay off short-term bills and to internally finance the growth of their business. Without additional working capital, a company may have to borrow additional funds from a bank or turn to investment bankers to raise more money. Negative working capital means assets aren’t being used effectively, and a company may a liquidity crisis. Even if a company has lots invested in fixed assets, it will face financial challenges if liabilities come due too soon. This will lead to more borrowing, late payments to creditors and suppliers and, as a result, a lower corporate credit rating for the company.</p>
+                       </div>
+                   </div> 
+                   <div class="col-md-12">
+                       <div class="f-h1-head f-h3-head">
+                           <h3>When negative working capital is ok</h3>
+                           <p>Depending on the type of business, companies can have negative working capital and still do well. Examples are grocery stores like Walmart or fast-food chains like McDonald’s that can generate cash very quickly due to high inventory turnover rates and by receiving payment from customers in a matter of a few days. These companies need little working capital.<br>Products that are bought from suppliers are immediately sold to customers before the company even gets a chance to pays the vendor or supplier. In contrast, capital-intensive companies that manufacture heavy equipment and machinery usually can’t raise cash quickly, as they sell their products on a long-term payment basis. Since they can’t sell fast enough, cash won’t be available immediately during tough financial times, so having enough working capital is desirable.</p>
+                       </div>
+                   </div>                                                                              
+               </div>
+               <div class="row">
+                   <div class="col-md-12">
+                       <div class="typo-column">
+                           <h2>Things to Remember</h2>
+                           <p>
+                             <ul>
+                               <li class=".f-h1-head"><p style="font-size:15px;margin-bottom: 10px">If the ratio is less than one then they have negative working capital.</p></li>
+                               <li class=".f-h1-head"><p style="font-size:15px">A high working capital ratio isn't always a good thing, it could indicate that they have too much inventory or they are not investing their excess cash.</p></li>
+                             </ul>                             
+                           </p>
+                       </div>
+                   </div>
+               </div>                                              
+           </div>
+      </section>
+      <!-- =========================
+        END FEATURE TYPOGRAPHY SECTION
+      ============================== --> 
+
+      <!-- =========================
+        START FOOTER SECTION
+      ============================== -->
+      <?php include 'footer.php'; ?>
+      <!-- =========================
+        END FOOTER SECTION
+      ============================== -->
+
+      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+      <script src="js/jquery-1.11.3.min.js"></script>        
+      <!-- RS5.0 Core JS Files -->
+      <script type="text/javascript" src="revolution/js/jquery.themepunch.tools.min.js?rev=5.0"></script>
+      <script type="text/javascript" src="revolution/js/jquery.themepunch.revolution.min.js?rev=5.0"></script>
+      <script type="text/javascript" src="revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+      <script type="text/javascript" src="revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+      <script type="text/javascript" src="revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+      <script type="text/javascript" src="revolution/js/extensions/revolution.extension.parallax.min.js"></script>          
+      <!-- Include all compiled plugins (below), or include individual files as needed -->
+      <script src="js/bootstrap.min.js"></script>
+      <script src="js/menuzord.js"></script>
+      <script src="js/owl.carousel.min.js"></script>
+      <script src="js/jquery.counterup.js"></script> 
+      <script src="js/waypoints.min.js"></script> 
+      <script src="js/countdown.js"></script> 
+      <script src="js/parallax.min.js"></script>      
+      <script src="js/jquery.bootFolio.js"></script>
+      <script src="js/jquery.magnific-popup.js"></script>
+      <script src="https://maps.googleapis.com/maps/api/js"></script>       
+      <script src="js/jquery-ui.js"></script>
+      <script src="js/rev-function.js"></script>     
+      <script src="js/smoothscroll.js"></script>      
+      <script src="js/jquery.matchHeight.js"></script>       
+      <script src="js/main.js"></script>       
+   </body>
+</html>
