e42ed2e609ebad0bae4be25c642c38e36bedfa2b
madhur1807 <madhurmittal1807@gmail.com>
2018-03-08 22:12:35 +0530

    ..

--- contact.php
+++ contact.php
@@ -190,7 +190,7 @@
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3499.2241247701545!2d77.13500441446826!3d28.71284728719354!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d016440000001%3A0x854cee01b3707864!2sAvusys+India+Pvt+Ltd!5e0!3m2!1sen!2sin!4v1520243728516" style="width: 100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
          </div>
          <div class="col-md-6" style="padding-right: 0px;border-top: 2px solid black" id="div-2">
-            <h2 style="text-align: center;">Corporate Office</h2><h5 style="text-align: center;">509-10511, Pearls Best Heights-II, Netaji Subash Place, Pitampura,(Near Max Hospital) New Delhi-110034</h5>
+            <h2 style="text-align: center;">Corporate Office</h2><h5 style="text-align: center;">509-10-11, Pearls Best Heights-II, Netaji Subash Place, Pitampura,(Near Max Hospital) New Delhi-110034</h5>
             <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11111.423799456077!2d77.15379037366087!3d28.69325537612982!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2b3b5f46551280d8!2sAvosys+Finance!5e0!3m2!1sen!2sin!4v1520245429594" style="width: 100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
          </div>
       </div>
